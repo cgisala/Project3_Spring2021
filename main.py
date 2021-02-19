2a977dc3b9784458bfc662b72ba13a3314e2e21a
chris <cgisala@msn.com>
2021-02-19 04:51:48 -0600

    added from view import*

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # has high-level config, sets up View, ViewModel, and SQLDatabase
 
 # Start View
+# High-level setup
 
-# To do set up View
+from view import *
+
 # To do import from sqldatabase
 # To do import view
 
